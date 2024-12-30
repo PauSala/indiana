dc9f8ce713dfc54eb7179415b10ef718bca8ec97
pausala <pau.salaf@gmail.com>
2024-12-30 20:09:16 +0100

    remove-unused-error

--- src/error.rs
+++ src/error.rs
@@ -2,9 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum MoleError {
-    #[error("An error occurred while parsing arguments: {0}")]
-    ArgParseError(#[from] clap::Error),
-
     #[error("IoError: {0}")]
     IoError(#[from] std::io::Error),
 }
