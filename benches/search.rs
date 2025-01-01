a3c48e116419e78dbcd9580bf36a7284b9c1861e
pausala <pau.salaf@gmail.com>
2025-01-01 12:42:43 +0100

    .env

--- benches/search.rs
+++ benches/search.rs
@@ -1,9 +1,11 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use dotenv::dotenv;
 use hashbrown::HashMap;
 use mole::{
     cli::Args,
     file_explorer::{self, CargoFiles},
 };
+use std::env;
 use std::{hint::black_box, path::PathBuf};
 
 fn search(args: &Args) -> Result<HashMap<String, CargoFiles>, mole::error::MoleError> {
@@ -11,11 +13,13 @@ fn search(args: &Args) -> Result<HashMap<String, CargoFiles>, mole::error::MoleE
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    dotenv().ok();
+    let path = PathBuf::from(env::var("DEFAULT_BENCHES_FOLDER").unwrap());
     c.bench_function("search", |b| {
         b.iter(|| {
             search(black_box(&Args {
                 name: "serde".to_string(),
-                path: PathBuf::from("/Users/pausala/.cargo"),
+                path: path.clone(),
                 deep: true,
                 threaded: true,
             }))
