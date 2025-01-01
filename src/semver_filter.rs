fee953c05901f5e20cbfd74ec5e4a35905db138f
pausala <pau.salaf@gmail.com>
2025-01-01 23:42:26 +0100

    docs

--- src/semver_filter.rs
+++ src/semver_filter.rs
@@ -1,6 +1,5 @@
-use semver::{Version, VersionReq};
-
 use crate::parser::data::OutputRow;
+use semver::{Version, VersionReq};
 
 pub fn filter(filter: Option<VersionReq>, data: Vec<OutputRow>) -> Vec<OutputRow> {
     data.into_iter()
